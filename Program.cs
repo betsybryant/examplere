642e82498bef21d8d91027fb676b1afe426858c0
Betsy Bryant <betsybryant@Betsys-MacBook-Air.local>
2021-09-03 00:34:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace examplere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
